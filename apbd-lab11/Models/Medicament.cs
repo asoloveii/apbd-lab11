f2271aaa157f28bae9e57b835e2052bc706f6a09
Arseni Salavei <arsenisoloveii@gmail.com>
2025-05-25 19:16:08 +0200

    feat: implemented models according to db scheme

---
+++ apbd-lab11/Models/Medicament.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_lab11.Models;
+
+public class Medicament
+{
+    public int IdMedicament { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+}
