b6697e87782891c6148782987d8e480d1b050562
Arseni Salavei <arsenisoloveii@gmail.com>
2025-05-25 19:21:25 +0200

    feat: completed dtos

---
+++ apbd-lab11/DTOs/CreatePrescriptionRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace apbd_lab11.DTOs;
+
+public class CreatePrescriptionRequest
+{
+    public PatientDto Patient { get; set; }
+    public int DoctorId { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+    public List<PrescriptionMedicamentDto> Medicaments { get; set; }
+}
+
+public class PatientDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+}
+
+public class PrescriptionMedicamentDto
+{
+    public int MedicamentId { get; set; }
+    public int Dose { get; set; }
+    public string Description { get; set; }
+}
---
+++ apbd-lab11/DTOs/PatientDetailsResponse.cs
@@ -0,0 +1,32 @@
+﻿namespace apbd_lab11.DTOs;
+
+public class PatientDetailsResponse
+{
+    public int IdPatient { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<PrescriptionDto> Prescriptions { get; set; }
+}
+
+public class PrescriptionDto
+{
+    public int IdPrescription { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+    public DoctorDto Doctor { get; set; }
+    public List<MedicamentDto> Medicaments { get; set; }
+}
+
+public class DoctorDto
+{
+    public int IdDoctor { get; set; }
+    public string FirstName { get; set; }
+}
+
+public class MedicamentDto
+{
+    public int IdMedicament { get; set; }
+    public string Name { get; set; }
+    public int Dose { get; set; }
+    public string Description { get; set; }
+}
