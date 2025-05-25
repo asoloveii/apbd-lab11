5add9c218390a069193b256a7f5fb825f39804b0
Arseni Salavei <arsenisoloveii@gmail.com>
2025-05-25 19:55:34 +0200

    feat: controllers

---
+++ apbd-lab11/Controllers/PatientsController.cs
@@ -0,0 +1,24 @@
+﻿using apbd_lab11.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_lab11.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PatientsController : ControllerBase
+{
+    private readonly IPatientsService _service;
+
+    public PatientsController(IPatientsService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPatientDetails(int id)
+    {
+        var result = await _service.GetPatientDetailsAsync(id);
+        if (result == null) return NotFound();
+        return Ok(result);
+    }
+}
---
+++ apbd-lab11/Controllers/PrescriptionsController.cs
@@ -0,0 +1,31 @@
+﻿using apbd_lab11.DTOs;
+using apbd_lab11.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_lab11.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PrescriptionsController : ControllerBase
+{
+    private readonly IPrescriptionsService _service;
+
+    public PrescriptionsController(IPrescriptionsService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPrescription([FromBody] CreatePrescriptionRequest request)
+    {
+        try
+        {
+            await _service.AddPrescriptionAsync(request);
+            return Ok("Prescription created.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
